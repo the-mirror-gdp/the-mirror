384bc4d99ed1652e8367570d70032ca1c3b3fe18
Gordon MacPherson <gordon@gordonite.tech>
2024-04-15 23:22:04 +0100

    android working

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,6 +5,7 @@ cd godot-engine
 # export ANDROID_HOME=/home/gordon/Android/Sdk
 export ANDROID_HOME=/Users/gordon/Library/Android/sdk
 #scons platform=android target=template_release arch=arm32
+scons target=editor
 scons platform=android ndk_platform=android-24 target=template_release arch=arm64 verbose=yes
 cd platform/android/java
 # On Linux and macOS
