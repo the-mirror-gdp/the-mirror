44b66ed8bdb8198d9307379bb734deffc72d805b
RevoluPowered <gordon@gordonite.tech>
2024-04-15 01:18:26 +0100

    fix cross compilation

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,6 +2,6 @@ cd godot-engine
 # we enable a compile database so we can easily debug in vscode and or other tools
 # scons target=editor optimize=speed debug_symbols=no compiledb=yes production=true
 
-export ANDROID_HOME=/c/Users/Gordon/AppData/Local/Android/Sdk
+export ANDROID_HOME=/home/gordon/Android/Sdk
 scons platform=android target=template_release arch=arm32
 scons platform=android target=template_release arch=arm64
