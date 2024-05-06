d336e5ee9c51e623c2d2eef9c0386daf22817ab5
Jared McCluskey <jared@themirror.space>
2024-05-06 14:50:13 -0700

    Update docusaurus.config.js (#157)

--- mirror-docs/docusaurus.config.js
+++ mirror-docs/docusaurus.config.js
@@ -166,7 +166,7 @@ const config = {
         indexName: 'themirror',
 
         // Optional: see doc section below
-        contextualSearch: true,
+        contextualSearch: false,
 
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         externalUrlRegex: 'themirror.space|docs.themirror.space',
