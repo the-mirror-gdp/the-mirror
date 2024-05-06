5436263d26a9535f265975798fd62c5c2f46d0b9
Jared McCluskey <jared@themirror.space>
2024-05-06 12:18:11 -0700

    Add Algolia (#154)

--- mirror-docs/docusaurus.config.js
+++ mirror-docs/docusaurus.config.js
@@ -156,6 +156,38 @@ const config = {
         textColor: '#06011F',
         isCloseable: true,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'WZDUGEW4O1',
+
+        // Public API key: it is safe to commit it
+        apiKey: 'cee727fb358dbae0ef4cb1c3f4b1da52',
+
+        indexName: 'themirror',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'themirror.space|docs.themirror.space',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        insights: true,
+
+        //... other Algolia params
+      },
       prism: {
         theme: lightCodeTheme,
         darkTheme: {
