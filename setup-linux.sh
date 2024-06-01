1fba06831db365bad90ccd97b671e0c2c993b99e
ph4n70m-nuk3r <ph4n70m.nuk3r@gmail.com>
2024-06-01 13:39:56 +0100

    Amend/improve linux setup script message.

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -o "MirrorGodotEditorLinux.x86_64" "https://storage.googleapis.com/mirror_native_client_builds/Engine/latest-editor/MirrorGodotEditorLinux.x86_64.zip"
-echo "If you do not have wget installed, you can install it or download from here https://storage.googleapis.com/mirror_native_client_builds/Engine/latest-editor/MirrorGodotEditorLinux.x86_64.zip"
+echo "If you do not have curl installed, you can either install it and retry, or download the latest editor from https://storage.googleapis.com/mirror_native_client_builds/Engine/latest-editor/MirrorGodotEditorLinux.x86_64.zip"
