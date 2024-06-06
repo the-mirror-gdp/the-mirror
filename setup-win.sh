d5297d1b716eab6f6532f15203105094cd593526
Gordon MacPherson <gordon@gordonite.tech>
2024-06-06 18:38:27 +0100

    Fix editor version and download links (#198)
    * Fix editor version and download links
    
    * Update download links in readme.md
    
    * Fix editor URL in the CICD

--- setup-win.sh
+++ setup-win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -o "MirrorGodotEditorWindows.exe" "https://storage.googleapis.com/mirror_native_client_builds/Engine/latest-editor/MirrorGodotEditorWindows.exe.zip"
-echo "If you do not have curl installed, you can install it or download from here https://storage.googleapis.com/mirror_native_client_builds/Engine/latest-editor/MirrorGodotEditorWindows.exe.zip"
+curl -o "MirrorGodotEditorWindows.exe" "https://storage.googleapis.com/mirror_native_client_builds/Engine/f2020817/MirrorGodotEditorWindows.exe"
+echo "If you do not have curl installed, you can install it or download from here https://storage.googleapis.com/mirror_native_client_builds/Engine/f2020817/MirrorGodotEditorWindows.exe"
