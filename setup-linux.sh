02ffe7a56ce743b0d1cbc28120931bb58d2e93fc
RevoluPowered <gordon@gordonite.tech>
2024-07-23 18:12:22 +0100

    Upgrade engine hash again

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -o "MirrorGodotEditorLinux.x86_64" "https://storage.googleapis.com/mirror_native_client_builds/Engine/4154a756/MirrorGodotEditorLinux.x86_64"
+curl -o "MirrorGodotEditorLinux.x86_64" "https://storage.googleapis.com/mirror_native_client_builds/Engine/35021301/MirrorGodotEditorLinux.x86_64"
--- setup-mac.sh
+++ setup-mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -o "MirrorGodotEditorMac.app.zip" "https://storage.googleapis.com/mirror_native_client_builds/Engine/4154a756/MirrorGodotEditorMac.app.zip"
+curl -o "MirrorGodotEditorMac.app.zip" "https://storage.googleapis.com/mirror_native_client_builds/Engine/35021301/MirrorGodotEditorMac.app.zip"
