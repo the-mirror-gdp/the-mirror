c18160b49bd6601c365a72c877baf40f0ccfc060
Gordon MacPherson <gordon@gordonite.tech>
2024-08-02 16:03:12 +0100

    Add forced login code for VR

--- mirror-godot-app/scripts/net/auth.gd
+++ mirror-godot-app/scripts/net/auth.gd
@@ -2,12 +2,19 @@ class_name MirrorAuth
 extends MirrorHttpClient
 
 enum {
-	POST_AUTH_USER_CREATE
+	POST_AUTH_USER_CREATE,
+	POST_CHECK_LOGIN_CODE
 }
 
 
 func create_account_if_missing() -> Promise:
 	return self.post_request(POST_AUTH_USER_CREATE, "/auth/auth-user-create", {})
+
+
+func check_login_code(login_code: String) -> Promise:
+	return self.post_request(POST_CHECK_LOGIN_CODE, "/login-code/check-login-code", {
+		"loginCode": login_code
+	})
 
 
 func _promise_fulfill_successful(request: Dictionary, promise: Promise) -> void:
