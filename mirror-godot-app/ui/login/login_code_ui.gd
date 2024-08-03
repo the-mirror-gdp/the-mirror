57b07e402ec040ee3371d8832df9f8cbd876619b
Gordon MacPherson <gordon@gordonite.tech>
2024-08-03 04:33:28 +0100

    Add virtual keyboard

--- mirror-godot-app/ui/login/login_code_ui.gd
+++ mirror-godot-app/ui/login/login_code_ui.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var login_code = $VBoxContainer/SignInMenu/MarginContainer/LoginContainer/LoginCode
 
 func _on_sign_in_pressed() -> void:
+	DisplayServer.virtual_keyboard_hide()
 	if login_code.text.is_empty():
 		return
 	var session_promise = Net.mirror_auth_client.check_login_code(login_code.text)
@@ -16,6 +17,13 @@ func _on_sign_in_pressed() -> void:
 	assert(data.loginCode)
 	print("login code data: ", data)
 	await LoginService.login_as_user_deeplink("", data.refreshToken, data.spaceId, get_tree() )
+
+
+func _gui_input_for_login_code(event: InputEvent):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			DisplayServer.virtual_keyboard_show(login_code.text)
+
 
 func _ready():
 	var login_code: bool = ProjectSettings.get_setting("feature_flags/force_enable_login_code", false)
