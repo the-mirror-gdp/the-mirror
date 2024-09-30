ef0cf4bf857f9c1ab97c33d95b457960ccfbf186
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-09-30 07:22:46 -0700

    name and redirect

--- mirror-2/app/build/layout.tsx
+++ mirror-2/app/build/layout.tsx
@@ -43,7 +43,7 @@ export default function Dashboard() {
           <div className="flex h-14 items-center px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
-              <span className="">Acme Inc</span>
+              <span className="">The Mirror</span>
             </Link>
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
--- mirror-2/next.config.js
+++ mirror-2/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: () => [
+    {
+      source: "/",
+      destination: "/build",
+      permanent: false,
+    },
+  ],
+};
 
 module.exports = nextConfig;
