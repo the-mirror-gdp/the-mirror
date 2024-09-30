643947c72a7b0a254fa990cef76391e32192fb6d
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-09-29 23:11:39 -0700

    text

--- mirror-2/app/layout.tsx
+++ mirror-2/app/layout.tsx
@@ -51,17 +51,6 @@ export default function RootLayout({
               </div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
                 <ThemeSwitcher />
               </footer>
             </div>
