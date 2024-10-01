ca8d72911810da086fc67703f192e17be024a970
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-01 13:56:12 -0700

    clean

--- mirror-2/components/ui/ui-sounds.tsx
+++ mirror-2/components/ui/ui-sounds.tsx
@@ -1,27 +1,23 @@
 // Separate hooks to manage UI audio with state so that the UI components are agnostic to it
 "use client"
-import { atom, useAtom, useAtomValue } from "jotai";
-import { cloneElement, isValidElement, useEffect, useRef, useState } from "react";
+import { useAppSelector } from "@/hooks/hooks";
+import { selectUiSoundsCanPlay } from "@/state/local";
+import { atomWithStorage } from 'jotai/utils';
+import { useEffect, useState } from "react";
 import useSound from "use-sound";
-import { atomWithStorage } from 'jotai/utils'
 
 export const uiSoundsCanPlayAtom = atomWithStorage('uiSoundsCanPlay', true);
 
-export function useAudioCanPlay() {
-  const [uiSoundsCanPlay, setUiSoundsCanPlay] = useAtom(uiSoundsCanPlayAtom);
-
-  return { uiSoundsCanPlay, setUiSoundsCanPlay };
-}
-
 export function useUiHoverSoundEffect() {
-  const audioCanPlayUserSetting = useAtomValue(uiSoundsCanPlayAtom)
+  const uiSoundsCanPlay = useAppSelector(selectUiSoundsCanPlay);
+
   const [canPlayAudioFromUserGestureTrack, setCanPlayAudioFromUserGestureTrack] = useState(false);
   const [playSound] = useSound("/sounds/hover_menu.wav", {
     volume: 0.05,
     interrupt: true
   });
   const play = function () {
-    if (audioCanPlayUserSetting && canPlayAudioFromUserGestureTrack) {
+    if (uiSoundsCanPlay && canPlayAudioFromUserGestureTrack) {
       playSound();
     }
   }
