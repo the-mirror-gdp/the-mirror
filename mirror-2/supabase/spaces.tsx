8bab62bf757acde6871546fc291f74f94007fae1
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-01 03:14:36 -0700

    cache pt 1

--- mirror-2/supabase/spaces.tsx
+++ mirror-2/supabase/spaces.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { createSupabaseBrowserClient } from "@/utils/supabase/client";
+import { cache } from "react";
 
 // we want to use client-side only because supabase tracks auth clientside
 
 
-export const getSingleSpaceAction = async (spaceId: string) => {
+export const getSingleSpaceAction = cache(async (spaceId: string) => {
   const supabase = createSupabaseBrowserClient();
 
   const { data, error } = await supabase
@@ -20,4 +21,4 @@ export const getSingleSpaceAction = async (spaceId: string) => {
   }
   console.log('sb data', data)
   return { data };
-};
+})
