756a27911d36301825ce4ceb04ee77134535b377
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-05 00:56:02 -0700

    dropzone

--- mirror-2/app/space/[spaceId]/build/(controlBar)/inner-control-bar.tsx
+++ mirror-2/app/space/[spaceId]/build/(controlBar)/inner-control-bar.tsx
@@ -1,4 +1,5 @@
-"use client"
+'use client';
+
 import Assets from "@/app/space/[spaceId]/build/(controlBar)/assets";
 import Code from "@/app/space/[spaceId]/build/(controlBar)/code";
 import Database from "@/app/space/[spaceId]/build/(controlBar)/database";
@@ -7,16 +8,39 @@ import Scenes from "@/app/space/[spaceId]/build/(controlBar)/scenes";
 import Settings from "@/app/space/[spaceId]/build/(controlBar)/settings";
 import { SkeletonCard } from "@/app/space/[spaceId]/build/(controlBar)/skeleton-card";
 import Versions from "@/app/space/[spaceId]/build/(controlBar)/versions";
+import { useGetFileUpload } from "@/hooks/file-upload";
 import { useAppSelector } from "@/hooks/hooks";
 import { selectControlBarCurrentView } from "@/state/local";
 import { Suspense } from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function InnerControlBar() {
   const currentView = useAppSelector(selectControlBarCurrentView);
 
+  // File dropzone
+  const onDrop = useGetFileUpload();
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    noClick: true,
+    noKeyboard: true,
+    onDrop,
+  });
+
   return (
-    <div className="p-2 m-2">
-      <Suspense fallback={SkeletonCard()}>
+    <div className="relative p-2 m-2" {...getRootProps()}>
+      {/* Input element for handling file uploads */}
+      <input {...getInputProps()} />
+
+      {/* Overlay that is shown when a file is dragged over the area */}
+      <div
+        className={`absolute inset-0 bg-gradient-to-b from-black via-primary to-black bg-opacity-30 ${isDragActive ? 'opacity-80 visible' : 'opacity-0 invisible'
+          } transition-opacity duration-300 ease-in-out flex justify-center items-center z-10`}
+      >
+        <p className="text-white font-semibold text-lg mt-5">Drop your file here</p>
+      </div>
+
+      {/* Content of the control bar */}
+      <Suspense fallback={<SkeletonCard />}>
         <span className={`${currentView === "scenes" ? "" : "hidden"}`}><Scenes /></span>
         <span className={`${currentView === "hierarchy" ? "" : "hidden"}`}><Hierarchy /></span>
         <span className={`${currentView === "assets" ? "" : "hidden"}`}><Assets /></span>
@@ -25,7 +49,6 @@ export default function InnerControlBar() {
         <span className={`${currentView === "versions" ? "" : "hidden"}`}><Versions /></span>
         <span className={`${currentView === "settings" ? "" : "hidden"}`}><Settings /></span>
       </Suspense>
-
-    </div >
+    </div>
   );
 }
