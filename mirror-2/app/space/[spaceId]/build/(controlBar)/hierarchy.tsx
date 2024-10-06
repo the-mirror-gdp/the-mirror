dbcc8e8846c6471470b8595fa1a641bb369d8f26
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-05 17:28:07 -0700

    clean

--- mirror-2/app/space/[spaceId]/build/(controlBar)/hierarchy.tsx
+++ mirror-2/app/space/[spaceId]/build/(controlBar)/hierarchy.tsx
@@ -170,7 +170,6 @@ export default function ControlledDemo() {
           togglerTemplate={togglerTemplate}
           dragdropScope="hierarchy"
           onDragDrop={(e) => {
-            const droppedNode = e.dragNode;
             const parentKey = e.dropNode.key as string
             // Expand the parent of the dropped node
             setExpandedKeys((prevExpandedKeys) => ({
