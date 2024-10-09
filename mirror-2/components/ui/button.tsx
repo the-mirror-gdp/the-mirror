33227129fcb65663da562097fd3c98caf7e9b33d
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-09 15:56:02 -0700

    polish buttons

--- mirror-2/components/ui/button.tsx
+++ mirror-2/components/ui/button.tsx
@@ -5,12 +5,11 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 duration-100",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90 delay-0",
-        // accent: "bg-accent text-muted-foreground font-bold hover:bg-accent/90 delay-0",
+        default: "bg-primary text-primary-foreground hover:bg-accent/90 delay-0",
 
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
