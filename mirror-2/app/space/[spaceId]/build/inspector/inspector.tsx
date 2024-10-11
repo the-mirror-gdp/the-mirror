e79a5090c9296aa4167589b531b7d93003a54145
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-10 17:41:55 -0700

    fix space viewport

--- mirror-2/app/space/[spaceId]/build/inspector/inspector.tsx
+++ mirror-2/app/space/[spaceId]/build/inspector/inspector.tsx
@@ -7,31 +7,33 @@ import { getCurrentEntity } from "@/state/local";
 import { cn } from "@/utils/cn";
 import { z } from 'zod'; // Import zod for validation
 
-export default function Inspector() {
+export default function Inspector({ className }) {
   const entity = useAppSelector(getCurrentEntity)
 
   const formSchema = z.object({
     name: z.string().min(3, { message: "Scene name must be at least 1 character long" }),
   });
 
-  return <div className={"p-2"}>
-    {entity && <TwoWayInput
-      id={entity.id}
-      fieldName="name"
-      formSchema={formSchema} // Your Zod validation schema
-      defaultValue={entity.name}
-      generalEntity={entity}
-      useGeneralGetEntityQuery={useGetSingleEntityQuery}
-      useGeneralUpdateEntityMutation={useUpdateEntityMutation}
-      renderComponent={(field) => (
-        <Input
-          type="text"
-          autoComplete="off"
-          className={cn("dark:bg-transparent border-none shadow-none  text-white")} // Apply className prop here
-          {...field}
-        />
-      )}
-    />
-    }
+  return <div className={cn(className)}>
+    {entity &&
+      <TwoWayInput
+        id={entity.id}
+        fieldName="name"
+        formSchema={formSchema} // Your Zod validation schema
+        defaultValue={entity.name}
+        generalEntity={entity}
+        useGeneralGetEntityQuery={useGetSingleEntityQuery}
+        useGeneralUpdateEntityMutation={useUpdateEntityMutation}
+        renderComponent={(field) => (
+          <Input
+            type="text"
+            autoComplete="off"
+            className={cn("dark:bg-transparent border-none shadow-none  text-white")} // Apply className prop here
+            {...field}
+          />
+        )}
+      />}
   </div>
+
+
 }
--- mirror-2/app/space/[spaceId]/build/layout.tsx
+++ mirror-2/app/space/[spaceId]/build/layout.tsx
@@ -13,13 +13,29 @@ export default function Layout({ children, params }: {
   return (
     <div className="h-screen max-h-screen w-screen grid grid-rows-[auto,1fr] overflow-hidden">
       <TopNavbar />
-      <div className="grid grid-cols-[minmax(100px,4%),minmax(200px,3fr),10fr,minmax(200px,3fr)] h-full overflow-hidden">
-        <Sidebar />
-        <InnerControlBar />
-        <SpaceViewport />
-        <Inspector />
+      {/* <div className="grid grid-cols-[250px,1fr,300px,1fr] h-full overflow-hidden"> */}
+      <div className="flex h-full overflow-hidden">
+        {/* Sidebar with fixed width */}
+        <div className="flex-none w-24">
+          <Sidebar />
+        </div>
 
-        {/* only loading children for separate data fetching to keep this as server component */}
+        {/* Inner control bar takes flexible space */}
+        <div className="flex-initial min-w-64">
+          <InnerControlBar />
+        </div>
+
+        {/* Space viewport (main content) */}
+        <div className="flex-auto">
+          <SpaceViewport />
+        </div>
+
+
+        {/* Instead of a div wrapping here, passing in className so that this component can be server compoonent; the Inspector has to use a hook for checking if entity selected */}
+        <Inspector className="flex-initial min-w-96" />
+
+
+        {/* Children for additional content */}
         {children}
       </div>
     </div>
