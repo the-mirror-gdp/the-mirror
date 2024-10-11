af88a24d51b5af1a003e033c502ec33bc5cc4be9
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-11 15:45:53 -0700

    inspector entity name

--- mirror-2/app/space/[spaceId]/build/inspector/inspector.tsx
+++ mirror-2/app/space/[spaceId]/build/inspector/inspector.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/utils/cn";
 export default function Inspector({ className }) {
   const entity = useAppSelector(getCurrentEntity)
 
-  return <div className={cn(className, "mt-1 p-2")}>
+  return <div className={cn(className, "p-3")}>
     {entity && <EntityFormGroup entity={entity} />}
   </div>
 
