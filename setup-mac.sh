383ca8fe87d2aac5a9afcdd35c42708079a691c6
Gordon MacPherson <gordon@gordonite.tech>
2024-10-11 17:33:16 +0100

    Upgrade Engine to godot 4.3-stable including project resources and CICD
    This also upgrades the OpenXR tools we use on Meta Quest 3.

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -o "MirrorGodotEditorMac.app.zip" "https://storage.googleapis.com/mirror_native_client_builds/Engine/a6443c6c/MirrorGodotEditorMac.app.zip"
+curl -o "MirrorGodotEditorMac.app.zip" "https://storage.googleapis.com/mirror_native_client_builds/Engine/2ed330c2/MirrorGodotEditorMac.app.zip"
