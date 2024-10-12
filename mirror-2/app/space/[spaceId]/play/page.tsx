ae795f8b2022685d102de28fcbc9728d97d2b300
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-12 15:24:49 -0700

    fix play mode

--- mirror-2/app/space/[spaceId]/play/page.tsx
+++ mirror-2/app/space/[spaceId]/play/page.tsx
@@ -1,5 +1,9 @@
-
+import SpaceViewport from "@/components/engine/space-viewport";
 
 export default function PlayPage() {
-  return <></>
+  console.log('play')
+  debugger
+  return <>
+    <SpaceViewport mode="play" pcImportId="8aef42cc-d6df-43da-89b9-c935a26306fa" />
+  </>
 }
--- mirror-2/app/space/[spaceId]/play/play.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import SpaceViewport from "@/components/engine/space-viewport"
-
-export function Play() {
-  return <SpaceViewport />
-}
