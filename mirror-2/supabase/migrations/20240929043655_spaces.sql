527044ca9de6ede0ea0e37c348bdd8209dfca0b2
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-13 00:52:24 -0700

    change asset, scene, space id to bigint unique

--- mirror-2/supabase/migrations/20240929043655_spaces.sql
+++ mirror-2/supabase/migrations/20240929043655_spaces.sql
@@ -1,5 +1,22 @@
+CREATE OR REPLACE FUNCTION generate_unique_id_spaces()
+RETURNS TRIGGER AS $$
+DECLARE
+    new_id BIGINT;
+BEGIN
+    LOOP
+        new_id := floor(random() * 9007198754740991 + 500000000)::BIGINT;
+        -- Check if the generated id already exists
+        IF NOT EXISTS (SELECT 1 FROM spaces WHERE id = new_id) THEN
+            NEW.id := new_id;
+            EXIT;
+        END IF;
+    END LOOP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 create table spaces (
-  id uuid not null primary key default uuid_generate_v4(),
+  id BIGINT PRIMARY KEY DEFAULT (floor(random() * 9007198754740991 + 500000000)::BIGINT), -- unique number instead of uuid since the game engine wants a number for this. set a floor above 500000000. 9007198754740991 is below the MAX_SAFE_INTEGER for js
   name text not null,
   description text,
   public_page_image_urls text[] default '{}',
@@ -9,6 +26,14 @@ create table spaces (
   updated_at timestamp with time zone not null default now(),
   constraint name_length check (char_length(name) >= 3)
   );
+
+-- Create the trigger to generate a unique id
+CREATE TRIGGER ensure_unique_id
+BEFORE INSERT ON spaces
+FOR EACH ROW
+WHEN (NEW.id IS NULL)
+EXECUTE FUNCTION generate_unique_id_spaces();
+
 
   -- add RLS
 alter table spaces
--- mirror-2/supabase/migrations/20241001224639_scenes.sql
+++ mirror-2/supabase/migrations/20241001224639_scenes.sql
@@ -1,11 +1,35 @@
+CREATE OR REPLACE FUNCTION generate_unique_id_scenes()
+RETURNS TRIGGER AS $$
+DECLARE
+    new_id BIGINT;
+BEGIN
+    LOOP
+        new_id := floor(random() * 9007198754740991 + 500000000)::BIGINT;
+        -- Check if the generated id already exists
+        IF NOT EXISTS (SELECT 1 FROM scenes WHERE id = new_id) THEN
+            NEW.id := new_id;
+            EXIT;
+        END IF;
+    END LOOP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 create table scenes (
-  id uuid not null primary key default uuid_generate_v4(),
-  space_id uuid references spaces on delete cascade not null, -- delete the scene if space is deleted
+  id BIGINT PRIMARY KEY DEFAULT (floor(random() * 9007198754740991 + 500000000)::BIGINT), -- unique number instead of uuid since the game engine wants a number for this. set a floor above 500000000. 9007198754740991 is below the MAX_SAFE_INTEGER for js
+  space_id BIGINT references spaces on delete cascade not null, -- delete the scene if space is deleted
   name text not null,
   created_at timestamp with time zone not null default now(),
   updated_at timestamp with time zone not null default now(),
   constraint name_length check (char_length(name) >= 0)
   );
+
+-- Create the trigger to generate a unique id
+CREATE TRIGGER ensure_unique_id
+BEFORE INSERT ON scenes
+FOR EACH ROW
+WHEN (NEW.id IS NULL)
+EXECUTE FUNCTION generate_unique_id_scenes();
 
   -- add RLS
 alter table scenes
