1fda85b7a68f3df36136bd2429c0a9f650d61c3b
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-15 09:52:10 -0700

    clean

--- mirror-2/app/page.tsx
+++ mirror-2/app/page.tsx
@@ -1,6 +1,4 @@
-import Hero from '@/components/hero'
-import { hasEnvVars } from '@/utils/supabase/check-env-vars'
-
 export default async function Index() {
-  return <>Temp, should be deleted</>
+  // should be deleted
+  return <></>
 }
