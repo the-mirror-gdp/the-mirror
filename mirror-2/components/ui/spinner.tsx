8cf5beab2cefda3d47b18014ed5010dda9aa4406
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-14 20:16:01 -0700

    engine/viewport clean

--- mirror-2/components/ui/spinner.tsx
+++ mirror-2/components/ui/spinner.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils'
 export function Spinner({ children = null, className, ...props }) {
   return (
     <LoaderCircle
-      className={cn('animate-pulse transition-all duration-100', className)}
+      className={cn('animate-pulse animate-spin transition-all', className)}
       {...props}
     >
       {children}
