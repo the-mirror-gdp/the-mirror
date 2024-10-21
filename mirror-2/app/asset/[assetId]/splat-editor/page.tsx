e8133e31a26c7d010a3f487144da01b05d58659b
Kluskey <jared.daniel.mccluskey@gmail.com>
2024-10-21 14:21:56 -0700

    comment

--- mirror-2/app/asset/[assetId]/splat-editor/page.tsx
+++ mirror-2/app/asset/[assetId]/splat-editor/page.tsx
@@ -3,7 +3,7 @@
 import SplatEditorViewport from '@/components/engine/splat-editor-viewport'
 import { useParams } from 'next/navigation'
 
-// blank page since we're using the parallel routes for spaceViewport, controlBar, etc.
+// feature flagged
 export default function Page() {
   const params = useParams<{ assetId: string }>()
   const assetId: number = parseInt(params.assetId, 10) // Use parseInt for safer conversion
